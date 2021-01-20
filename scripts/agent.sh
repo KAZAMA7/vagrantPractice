0605248e4db9ae1209a45efd775656c7b3a9a8c5
Giridhar Konda <giridhar.konda@experitest.com>
2021-01-20 20:22:56 +0530

    perfect setup of two nodes with ansible

--- scripts/agent.sh
+++ scripts/agent.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
+hostname agent.example.com
+mv /tmp/sshd_config /etc/ssh/sshd_config
+mkdir ~/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDncabVCF2+c/tulqSzu9Z+FZ0Lsi7s/PGAZejQSA68G99/I+yq8EKFiXr15qo0ilNATEq+oQIPjLTwlvgSRcZd/VxJ5ZgInqxgK+agxOs77aPpTCyhbV3IkthCr0bgo0/7YOP1zMVjI71RcB5bSyTUNH9plw1VG0lmnBWmtbtfTcrYqBUR+YDfhTQSmzJcSZF63eYbO872iKuLKViZHJBDiqvx4XCBGHrF+nrp+DR1FrylK6PImM8tp6CZ2cQvgncjbN+pJsFsCLumHgyym3UHRENIMTPtBbn7tG6V4Os/HV1Xg3hzrqW7m3YX5vckVxj6DsnqqoybjJP2hpXgn/Mh
+root@ansible.example.com" > ~/.ssh/authorized_keys
+systemctl restart sshd
 useradd ansible
 echo P@ssw0rld | passwd ansible --stdin
+echo P@ssw0rld | passwd root --stdin
 echo "192.168.111.2     ansible.example.com" >> /etc/hosts
 echo "192.168.111.3     agent.example.com" >> /etc/hosts
 
--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-
-yum update -y
+hostname ansible.example.com
 yum install epel-release -y
 yum install python-pip git openssh -y
 pip install ansible
+mv /tmp/ssh /root/.ssh
+chmod -R 600 /root/.ssh
+chown -R root:root /root/.ssh
+mv /tmp/sshd_config /etc/ssh/sshd_config
+systemctl restart sshd
 git config --global user.name "Giridhar Konda"
 git config --global user.email "giridhar0241@gmail.com"
 useradd ansible
 echo P@ssw0rld | passwd ansible --stdin
+echo P@ssw0rld | passwd root --stdin
+echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/ansible
 echo "192.168.111.2     ansible.example.com" >> /etc/hosts
 echo "192.168.111.3     agent.example.com" >> /etc/hosts
 
